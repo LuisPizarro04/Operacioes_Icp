d6f7d79cbc5dcae80b93db7da06f31e8173d31a3
Luis Pizarro <luispizarroalvarez8mail.com>
2023-05-08 22:00:35 -0400

    1er Commit: Directorio de aplicaciones y modulos

---
+++ gestion_campañas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestionCampañasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gestion_campañas'
---
+++ gestion_cierre_mes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestionCierreMesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gestion_cierre_mes'
